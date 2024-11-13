20e7e07d0da8e61568d4dea35fa7ee1dfae9a4dc
Apex16 <aarson9099@gmail.com>
2024-11-13 20:42:16 +0545

    files added

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Header from './header';
+import Main from './main';
+import Sidebar from './sidebar';
+function App() {
+  return (
+    <div>
+      <Header name="Aarson" color="purple"/>
+      <Main/>
+      <Sidebar/>
+      </div>
+  )
+}
+
+export default App;
---
+++ src/components/heading.js
@@ -0,0 +1,6 @@
+function Footer() {
+ return (<h1>This is an h1 footer</h1>
+ )
+  }
+  
+  export default Footer;
